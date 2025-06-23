fcb2ee482e0ba8ec0b82f6e7c0d888ae1ed77926
kariukialex999 <kariukialex999@gmail.com>
2025-06-23 15:08:39 +0300

    feat: Add specification to filter order items

--- src/main/java/com/kariioke/E_commerce/specifications/OrderItemSpecification.java
+++ src/main/java/com/kariioke/E_commerce/specifications/OrderItemSpecification.java
@@ -1,4 +1,34 @@
 package com.kariioke.E_commerce.specifications;
 
-public class OrderSpecification {
+import com.kariioke.E_commerce.entity.OrderItem;
+import com.kariioke.E_commerce.enums.OrderStatus;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDateTime;
+
+public class OrderItemSpecification {
+
+    //generates specification to filter item by status
+    public static Specification<OrderItem> hasStatus(OrderStatus status) {
+        return ((root, query, criteriaBuilder) -> status != null ? criteriaBuilder.equal(root.get("status"), status) : null);
+    }
+
+    //generates specification to filter items by data range
+    public static Specification<OrderItem> createdBetween(LocalDateTime startDate, LocalDateTime endDate) {
+        return ((root, query, criteriaBuilder) -> {
+            if(startDate != null && endDate != null) {
+                return criteriaBuilder.between(root.get("createdAt"), startDate, endDate);
+            }else if(startDate != null) {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate);
+            }else if(endDate != null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate);
+            }else {
+                return null;
+            }
+        });
+    }
+    //generates specification to filter item by id
+    public static Specification<OrderItem> hasItemId(Long itemId) {
+        return ((root, query, criteriaBuilder) -> itemId != null ? criteriaBuilder.equal(root.get("id"), itemId) : null);
+    }
 }
