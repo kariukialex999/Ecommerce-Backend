f5c12519d9645f77ff42525f949b290aa05cbe9f
kariukialex999 <kariukialex999@gmail.com>
2025-06-23 15:24:33 +0300

    feat: Add user service implementation

--- src/main/java/com/kariioke/E_commerce/service/interf/UserService.java
+++ src/main/java/com/kariioke/E_commerce/service/interf/UserService.java
@@ -1,4 +1,20 @@
 package com.kariioke.E_commerce.service.interf;
 
+import com.kariioke.E_commerce.dto.Response;
+import com.kariioke.E_commerce.dto.UserDto;
+import com.kariioke.E_commerce.entity.User;
+
 public interface UserService {
+
+    Response registerUser(UserDto registrationRequest);
+
+    Response loginUser(UserDto loginRequest);
+
+    Response getAllUsers();
+
+    User getLoginUser();
+
+    Response getUserInfoAndOrderHistory();
+
+
 }
